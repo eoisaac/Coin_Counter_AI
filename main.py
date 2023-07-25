6a7317f05153b3cdebff613ffd7f935081c287d3
Isaac Santiago <eoisaacc@gmail.com>
2023-07-25 19:29:45 -0300

    refactor: create CoinDetector class

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import cv2
+from coin_detector import CoinDetector
 
-video = cv2.VideoCapture(0)
 
-while True:
-    _, img = video.read()
-    img = cv2.resize(img, (640, 480))
-
-    cv2.imshow("IMG", img)
-    cv2.waitKey(1)
-
+if __name__ == "__main__":
+    coin_detector = CoinDetector()
+    coin_detector.capture_video()
